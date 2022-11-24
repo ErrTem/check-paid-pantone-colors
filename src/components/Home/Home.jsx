970455732e530c566b95ee0b9d68fa0348db283f
Artem <nitroxess@gmail.com>
2022-11-24 16:10:13 +0300

    added info and css normalize

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -31,14 +31,21 @@ let Home = (props) => {
 
   return (
     <div className={s.home}>
-      <form >
-        <div className={s.elem}>
-          <input id='input' type='text' placeholder='Write site adress' onChange={textInput} />
-        </div >
-        <div className={s.elem}>
-          <button onClick={handleSubmit}>Check it now !</button>
-        </div>
-      </form>
+      <div className={s.text}>
+        Hello there! <br />
+        Due to the new rules of pantone license <a href='https://www.theverge.com/2022/11/1/23434305/adobe-pantone-subscription-announcement-photoshop-illustrator'>
+          now you have to pay to use Pantone colors in Adobe products.
+        </a> Who knows what corporations come up with next?<br />
+        You can check the website for paid pantone colors by the form below
+      </div>
+
+      <div className={s.elem}>
+        <input id='input' type='text' placeholder='Write site adress' onChange={textInput} />
+      </div >
+      <div className={s.elem}>
+        <button onClick={handleSubmit}>Check it now !</button>
+      </div>
+
 
       <div>
         {props.wasAsked
